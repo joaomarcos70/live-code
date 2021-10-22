893a9e653737a8388a091dcecccb58bddd93005f
Joao Albuquerque <joaomarcos70@outlook.com.br>
2021-10-22 11:24:30 -0300

    configurações basicas de rota

--- api/index.js
+++ api/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
-
-
-app.use(express.json())
-
 const port = 3000
+
+routes(app)
 
 app.listen(port, ()=>{
     console.log("servidor rodando na porta :", port);   
